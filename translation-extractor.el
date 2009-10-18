f0fa92aab7434f2b218e5f0a61e324bd51df965e
wojtas <wojtas@debian.(none)>
2009-10-18 21:14:50 +0200

    Wrong keys and \n missing

--- translation-extractor.el
+++ translation-extractor.el
@@ -2,22 +2,23 @@
 ;;; TODO: fails if translated string contains \n
 ;;;
 
+
 (defconst translation-yml-file "~/devel/medical-reservation-system/mrs/config/locales/en.yml")
 
 (defconst what-is-in-string "-_A-Za-z0-9\n\t\b\s!@#$%^&*()-+=_")
 
 (defun select-string ()
   "Select a string under cursor. “string” here is anything between “”. Returns_pair_(beginig-of-string-in-buffer, end-of-string-in-buffer)."
- (interactive)
- (let (b1 b2)
-   (skip-chars-backward what-is-in-string)
-   (setq b1 (point))
-   (skip-chars-forward what-is-in-string)
-   (setq b2 (point))
-   (set-mark b1)
-   (cons b1 b2)
-   )
- )
+  (interactive)
+  (let (b1 b2)
+    (skip-chars-backward what-is-in-string)
+    (setq b1 (point))
+    (skip-chars-forward what-is-in-string)
+    (setq b2 (point))
+    (set-mark b1)
+    (cons b1 b2)
+    )
+  )
 
 (defun localize-selected-string ()
   "Repleces string under cursor to fit for rails translations e.g string \"Someone else\" will be converted to t(:someone_else). Returns translation list: key and translation "  
@@ -31,12 +32,13 @@
           (replace-string " " "_" `() p1 p2)
           (replace-string "\"" "t(:" `()  (- p1 1) p1)
           (replace-string "\"" ")"   `()  (+ p2 2) (+ p2 3) )
-          (cons (buffer-substring (+ p1 1) (+ p2 1)) val)
+          (cons (buffer-substring (+ p1 2) (+ p2 2)) val)
           )
         )
       )
     )
   )
+
 
 (defun update-translation-file ()
   "Replaces string under cursor and updates translation file"
@@ -54,12 +56,13 @@
             (message "Key %s exists in buffer %s" key (buffer-name))
           (progn
             (end-of-buffer)
+            (insert "\n")
             (insert key)
             (insert ": \"")
             (insert value)
-            (insert "\"")
-            (indent-for-tab-command)
-            )
+            (insert "\""))
+          (indent-for-tab-command)
+            
           )
         (switch-to-buffer buf-name)
         )
